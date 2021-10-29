1c41a62165811937107514a5d95bc6d6017e33f9
AntonioArce <tony.ipn26@gmail.com>
2021-10-29 14:22:41 -0500

    Archivos s de una suma, if y un ciclo for

---
+++ EnsambladorEjemplos/if.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int a = 10;
+    int b = 10;
+
+    if(a == b){
+        printf("BIEN \n");
+    }
+    
+    return 0;
+}
---
+++ EnsambladorEjemplos/suma.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    int a =10 ,b = 20;
+    int r = a + b;
+
+    printf("%d \n", r);
+
+    return 0;
+}
